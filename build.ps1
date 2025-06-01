2b0722a61e4a35e29de4660e54c9dac5eda8f6bc
TeamDman <TeamDman9201@gmail.com>
2025-06-01 14:09:55 -0400

    Fix build script by turning off arg that breaks dependencies builds

--- build.ps1
+++ build.ps1
@@ -1,6 +1,7 @@
 # https://github.com/johnthagen/min-sized-rust
 $old=$env:RUSTFLAGS
-$Env:RUSTFLAGS="-Zlocation-detail=none -Zfmt-debug=none"
+$Env:RUSTFLAGS="-Zlocation-detail=none"
+# $Env:RUSTFLAGS="-Zlocation-detail=none -Zfmt-debug=none"
 try {
     cargo build --release
 } finally {
